04767c5e11318d7a6a7db3f459317a209bb30935
Raoul Strackx <raoul.strackx@fortanix.com>
2024-09-26 15:41:10 +0200

    Bring your own crypto

--- az-cvm-vtpm/src/vtpm/verify.rs
+++ az-cvm-vtpm/src/vtpm/verify.rs
@@ -2,19 +2,70 @@
 // Licensed under the MIT License.
 
 use super::{Quote, QuoteError};
-use openssl::pkey::{PKey, Public};
-use openssl::{hash::MessageDigest, sha::Sha256, sign::Verifier};
 use thiserror::Error;
 use tss_esapi::structures::{Attest, AttestInfo};
 use tss_esapi::traits::UnMarshall;
+
+#[cfg(feature = "openssl")]
+mod openssl {
+    pub use openssl::pkey::{PKey, Public};
+    pub use openssl::sha::Sha256;
+    use openssl::{hash::MessageDigest, sign::Verifier};
+    use super::{RsaVerification, Sha256Hasher, VerifyError};
+
+    pub enum Rsa{}
+
+    impl RsaVerification for Rsa {
+        type PublicKey = PKey<Public>;
+
+        fn verify(data: &[u8], pub_key: &Self::PublicKey, signature: &[u8]) -> Result<(), VerifyError> {
+            let mut verifier = Verifier::new(MessageDigest::sha256(), pub_key).map_err(|_| VerifyError::DigestError)?;
+            verifier.update(&data).map_err(|_| VerifyError::DigestError)?;
+            let is_verified = verifier.verify(signature).map_err(|_| VerifyError::SignatureError)?;
+            if !is_verified {
+                Err(VerifyError::SignatureMismatch)
+            } else {
+                Ok(())
+            }
+        }
+    }
+
+    impl Sha256Hasher for Sha256 {
+        fn new() -> Self {
+            Sha256::new()
+        }
+
+        fn update(&mut self, data: &[u8]) {
+            self.update(data)
+        }
+
+        fn finish(self) -> [u8; 32] {
+            self.finish()
+        }
+    }
+}
+
+pub trait RsaVerification {
+    type PublicKey;
+
+    fn verify(data: &[u8], pkey: &Self::PublicKey, signature: &[u8]) -> Result<(), VerifyError>;
+}
+
+pub trait Sha256Hasher {
+    fn new() -> Self;
+    fn update(&mut self, data: &[u8]);
+    fn finish(self) -> [u8; 32];
+}
 
 #[non_exhaustive]
 #[derive(Error, Debug)]
 pub enum VerifyError {
     #[error("tss error")]
     Tss(#[from] tss_esapi::Error),
-    #[error("openssl error")]
-    OpenSsl(#[from] openssl::error::ErrorStack),
+    #[error("Digest computation error")]
+    DigestError,
+    #[error("Signature computation error")]
+    SignatureError,
     #[error("quote is not signed by key")]
     SignatureMismatch,
     #[error("nonce mismatch")]
@@ -33,15 +84,15 @@ impl Quote {
     /// * `pub_key` - A public key to verify the Quote's signature
     ///
     /// * `nonce` - A byte slice to verify the Quote's nonce
-    pub fn verify(&self, pub_key: &PKey<Public>, nonce: &[u8]) -> Result<(), VerifyError> {
-        self.verify_signature(pub_key)?;
+    pub fn verify<C: RsaVerification, H: Sha256Hasher>(&self, pub_key: &C::PublicKey, nonce: &[u8]) -> Result<(), VerifyError> {
+        self.verify_signature::<C>(pub_key)?;
 
         let quote_nonce = &self.nonce()?;
         if nonce != quote_nonce {
             return Err(VerifyError::NonceMismatch);
         }
 
-        self.verify_pcrs()?;
+        self.verify_pcrs::<H>()?;
 
         Ok(())
     }
@@ -51,19 +102,13 @@ impl Quote {
     /// # Arguments
     ///
     /// * `pub_key` - A public key to verify the Quote's signature
-    pub fn verify_signature(&self, pub_key: &PKey<Public>) -> Result<(), VerifyError> {
-        let mut verifier = Verifier::new(MessageDigest::sha256(), pub_key)?;
-        verifier.update(&self.message)?;
-        let is_verified = verifier.verify(&self.signature)?;
-        if !is_verified {
-            return Err(VerifyError::SignatureMismatch);
-        }
-        Ok(())
+    pub fn verify_signature<C: RsaVerification>(&self, pub_key: &C::PublicKey) -> Result<(), VerifyError> {
+        C::verify(&self.message, pub_key, &self.signature)
     }
 
     /// Verify that the TPM Quote's PCR digest matches the digest of the bundled PCR values
     ///
-    pub fn verify_pcrs(&self) -> Result<(), VerifyError> {
+    pub fn verify_pcrs<H: Sha256Hasher>(&self) -> Result<(), VerifyError> {
         let attest = Attest::unmarshall(&self.message)?;
         let AttestInfo::Quote { info } = attest.attested() else {
             return Err(VerifyError::Quote(QuoteError::NotAQuote));
@@ -72,7 +117,7 @@ impl Quote {
         let pcr_digest = info.pcr_digest();
 
         // Read hashes of all the PCRs.
-        let mut hasher = Sha256::new();
+        let mut hasher = H::new();
         for pcr in self.pcrs.iter() {
             hasher.update(pcr);
         }
@@ -88,7 +133,11 @@ impl Quote {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    #[cfg(feature = "openssl")]
+    use {
+        super::*,
+        super::openssl::*,
+    };
 
     // // Use this code to generate the scriptures for the test on an AMD CVM.
     //
@@ -113,7 +162,7 @@ mod tests {
     //     Ok(())
     // }
 
-    #[cfg(feature = "verifier")]
+    #[cfg(all(feature = "verifier", feature = "openssl"))]
     #[test]
     fn test_quote_validation() {
         // Can be retrieved by `get_ak_pub()` or via tpm2-tools:
@@ -132,13 +181,13 @@ mod tests {
 
         // proper nonce in message
         let nonce = "challenge".as_bytes().to_vec();
-        let result = quote.verify(&pkey, &nonce);
+        let result = quote.verify::<Rsa, Sha256>(&pkey, &nonce);
         assert!(result.is_ok(), "Quote verification should not fail");
 
         // wrong signature
         let mut wrong_quote = quote.clone();
         wrong_quote.signature.reverse();
-        let result = wrong_quote.verify(&pkey, &nonce);
+        let result = wrong_quote.verify::<Rsa, Sha256>(&pkey, &nonce);
         let error = result.unwrap_err();
         assert!(
             matches!(error, VerifyError::SignatureMismatch),
@@ -147,7 +196,7 @@ mod tests {
 
         // improper nonce
         let nonce = vec![1, 2, 3, 4];
-        let result = quote.verify(&pkey, &nonce);
+        let result = quote.verify::<Rsa, Sha256>(&pkey, &nonce);
         let error = result.unwrap_err();
         assert!(
             matches!(error, VerifyError::NonceMismatch),
@@ -156,10 +205,11 @@ mod tests {
     }
 
     #[test]
+    #[cfg(feature = "openssl")]
     fn test_pcr_values() {
         let quote_bytes = include_bytes!("../../test/quote.bin");
         let quote: Quote = bincode::deserialize(quote_bytes).unwrap();
-        let result = quote.verify_pcrs();
+        let result = quote.verify_pcrs::<Sha256>();
         assert!(result.is_ok(), "PCR verification should not fail");
     }
 }
